c3519efcffbb21da6a0c3c7e906cb8b94fc8e9d1
Arwin Tio <Arwin.tio@adroll.com>
2021-06-05 23:07:51 -0700

    Fix get all tickers

--- src/ticker.rs
+++ src/ticker.rs
@@ -1,6 +1,50 @@
 use client::*;
 use errors::*;
 use serde_json::from_str;
+use serde_json::value;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TickersTrading {
+    pub symbol: String,
+    pub bid: f64,
+    pub bid_size: f64,
+    pub ask: f64,
+    pub ask_size: f64,
+    pub daily_change: f64,
+    pub daily_change_perc: f64,
+    pub last_price: f64,
+    pub volume: f64,
+    pub high: f64,
+    pub low: f64
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TickersFunding {
+    pub symbol: String,
+    pub frr: f64,
+    pub bid: f64,
+    pub bid_period: i64,
+    pub bid_size: f64,
+    pub ask: f64,
+    pub ask_period: i64,
+    pub ask_size: f64,
+    pub daily_change: f64,
+    pub daily_change_relative: f64,
+    pub last_price: f64,
+    pub volume: f64,
+    pub high: f64,
+    pub low: f64,
+    pub _placeholder1: value::Value,
+    pub _placeholder2: value::Value,
+    pub frr_amount_available: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum TickersData {
+    TickersTrading(TickersTrading),
+    TickersFunding(TickersFunding),
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TradingPair {
@@ -67,11 +111,12 @@ impl Ticker {
         Ok(ticker)
     }
 
-    pub fn all_tickers<>(&self) -> Result<Vec<TradingPair>> {
+    pub fn all_tickers<>(&self) -> Result<Vec<TickersData>> {
         let endpoint: String = format!("tickers?symbols=ALL");
+
         let data = self.client.get(endpoint, String::new())?;
 
-        let ticker: Vec<TradingPair> = from_str(data.as_str())?;
+        let ticker: Vec<TickersData> = from_str(data.as_str())?;
 
         Ok(ticker)
     }
